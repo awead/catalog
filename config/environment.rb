68ead62ff5348eb6623501002f30230c00a50ecd
awead <amsterdamos@gmail.com>
2010-11-01 14:45:01 -0400

    installed ead plugin

--- config/environment.rb
+++ config/environment.rb
@@ -24,7 +24,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
-  # :all can be used as a placeholder for all plugins not explicitly named
+  :all
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
   # Skip frameworks you're not going to use. To use Rails without a database,
