083b4339e61912914495691326fc8c94ad3b6979
awead <amsterdamos@gmail.com>
2011-01-20 21:23:08 -0500

    customizations for rockhall.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,17 @@ require_dependency( 'vendor/plugins/blacklight/app/controllers/application_contr
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  
+  before_filter :add_local_assets
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  
+  protected
+  
+  def add_local_assets
+      stylesheet_links << "rockhall"
+      #javascript_includes << "my_js"
+  end
+
 end
