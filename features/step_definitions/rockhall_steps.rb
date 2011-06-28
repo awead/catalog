cc1965f7d9bdbba3996773d196201bbe025e39a4
awead <amsterdamos@gmail.com>
2011-06-28 16:15:55 -0400

    BL-51: faceting working as specified in test

--- features/step_definitions/rockhall_steps.rb
+++ features/step_definitions/rockhall_steps.rb
@@ -39,3 +39,7 @@ end
 Then /^I should see the field content "([^"]*)" contain "([^"]*)"$/ do |arg1, arg2|
   page.should have_xpath("//*/dd[contains(@class, arg1)]", :text => arg2)
 end
+
+Then /^I should see the field content "([^"]*)" not contain "([^"]*)"$/ do |arg1, arg2|
+  page.should_not have_xpath("//*/dd[contains(@class, arg1)]", :text => arg2)
+end
