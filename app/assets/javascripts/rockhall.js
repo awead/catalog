77b58959935a6944a19aaaf56e2609d02f33dd14
Adam Wead <amsterdamos@gmail.com>
2012-02-15 17:20:10 -0500

    BL-179: Adding js for status info

---
+++ app/assets/javascripts/rockhall.js
@@ -0,0 +1,30 @@
+// Rockhall Javascript
+// this is just a place-holder for now
+
+
+$(document).ready(returnStatus);
+
+
+function returnStatus() {
+
+
+  $('.innovative_status').each(function() {
+
+    var id  = $(this).attr("id");
+    var url = "holdings/" + id;
+
+    $.ajax({
+      url: url,
+      error: function(){
+        $('#'+id).append("Unknown");
+      },
+      success: function(data){
+        $('#'+id).append(data);
+      }
+    });
+
+
+  });
+
+
+}
