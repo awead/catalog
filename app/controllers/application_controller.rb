3f704d890e50fddd880ed2477951011c6af21db7
Adam Wead <amsterdamos@gmail.com>
2012-02-28 10:48:44 -0500

    Little bits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
   rescue_from ActionController::RoutingError, :with => :render_not_found
   rescue_from ActionController::UnknownController, :with => :render_not_found
-  rescue_from ActionController::UnknownAction, :with => :render_not_found
+  rescue_from AbstractController::ActionNotFound, :with => :render_not_found
 
   private
 
