605a92b33ba0c683e7d4ce8aedcf6efc8e35eb34
Adam Wead <amsterdamos@gmail.com>
2012-04-26 14:10:09 -0400

    BL-212: Enabling errors in development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,13 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
-  rescue_from Exception, :with => :render_error
-  rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
-  rescue_from ActionController::RoutingError, :with => :render_not_found
-  rescue_from ActionController::UnknownController, :with => :render_not_found
-  rescue_from AbstractController::ActionNotFound, :with => :render_not_found
+  unless Rails.env.match("development")
+    rescue_from Exception, :with => :render_error
+    rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
+    rescue_from ActionController::RoutingError, :with => :render_not_found
+    rescue_from ActionController::UnknownController, :with => :render_not_found
+    rescue_from AbstractController::ActionNotFound, :with => :render_not_found
+  end
 
   private
 
