8468857be1208cf2facea3dee2c668c6758d3777
Adam Wead <amsterdamos@gmail.com>
2012-04-26 15:27:28 -0400

    BL-212: Updating ComponentsController
    Added additional include statements for new configuration methods.

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -4,6 +4,9 @@ class ComponentsController < ApplicationController
   include CatalogHelper
   include Rockhall::EadSolrMethods
 
+  include Blacklight::Configurable
+  copy_blacklight_config_from(CatalogController)
+
   def index
     @documents = get_component_docs_from_solr(params[:ead_id], { :level => params[:component_level], :parent_ref => params[:parent_ref] })
   end
