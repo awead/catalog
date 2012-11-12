b6036fbded669a013838109223080f569cbf9033
Adam Wead <amsterdamos@gmail.com>
2012-11-12 16:12:58 -0500

    BL-240: Correcting a test

--- spec/helpers/holdings_helper_spec.rb
+++ spec/helpers/holdings_helper_spec.rb
@@ -11,7 +11,7 @@ describe HoldingsHelper  do
 
   describe ".status" do
     it "should return a status message for a given code" do
-      @test.status["o"].should == "Library Use Only"
+      @test.status["o"].should == "LIB USE ONLY"
       @test.status["p"].should == "In Process"    
     end
   end
