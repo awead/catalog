55848f724791d21d4bf0d4685a4004b42e38e6a6
Adam Wead <amsterdamos@gmail.com>
2012-12-06 16:20:24 -0500

    Create RESTful urls for archival items

--- app/helpers/local_blacklight_helper.rb
+++ app/helpers/local_blacklight_helper.rb
@@ -13,4 +13,19 @@ module LocalBlacklightHelper
     render :partial => "catalog/rockhall_heading"
   end
 
+  # This replaces Blacklight::BlacklightHelperBehavior.link_to_document to
+  # render a link to an archival item.  The behavior for all other formats
+  # is the same
+  def link_to_document(doc, opts={:label=>nil, :counter => nil, :results_view => true})
+    label ||= blacklight_config.index.show_link.to_sym
+    label = render_document_index_label doc, opts
+
+    if doc["format"] == "Archival Item"
+      id, refnum = doc["id"].split("ref")
+      link_to label, catalog_path([id,"ref"+refnum]), { :'data-counter' => opts[:counter] }.merge(opts.reject { |k,v| [:label, :counter, :results_view].include? k  })
+    else
+      link_to label, doc, { :'data-counter' => opts[:counter] }.merge(opts.reject { |k,v| [:label, :counter, :results_view].include? k  })
+    end
+  end
+
 end
