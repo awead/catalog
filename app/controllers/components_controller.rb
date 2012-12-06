91653205d0b91c59666bb0d70113771d6a6276ba
Adam Wead <amsterdamos@gmail.com>
2012-12-06 15:27:03 -0500

    Cleaning up ComponentsController

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -1,17 +1,9 @@
 class ComponentsController < ApplicationController
 
-  #include Rockhall::EadSolrMethods
   include Rockhall::ControllerBehaviors
   include Blacklight::SolrHelper
 
-  # Remove these if everything still works:
-
-  #include CatalogHelper
-  #include Blacklight::Configurable
-  #copy_blacklight_config_from(CatalogController)
-
   def index
-    #@documents = get_component_docs_from_solr(params[:ead_id], { :parent_ref => params[:parent_ref] })
     @documents = additional_ead_components(params[:ead_id], params[:parent_ref])
   end
 
