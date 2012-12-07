074b8dadc960eab4e42778de75382dff88ae3c81
Adam Wead <amsterdamos@gmail.com>
2012-12-07 12:53:53 -0500

    Adding start and row options to ComponentsController

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -3,7 +3,7 @@ class ComponentsController < ApplicationController
   include Rockhall::SolrHelperExtension
   
   def index
-    @documents = additional_ead_components(params[:ead_id], params[:parent_ref])
+    @documents = additional_ead_components(params[:ead_id], params[:parent_ref], {:start => params[:start], :rows => params[:rows]})
   end
 
 end
