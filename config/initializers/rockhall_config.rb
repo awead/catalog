b5df2cc968ce7426f25e6a67791fe8cb9afd8351
Adam Wead <amsterdamos@gmail.com>
2013-07-23 11:40:19 -0400

    BL-373: Local configuration should depend on Rails environment

--- config/initializers/rockhall_config.rb
+++ config/initializers/rockhall_config.rb
@@ -1,19 +1,19 @@
+RH_CONFIG = YAML.load_file("#{Rails.root.to_s}/config/rockhall.yml")[Rails.env]
+
 # setup up empty hash to put our configuration into
 config = Hash.new
 
 # IP of our OPAC
-config[:opac_ip] = "129.22.104.30"
+config[:opac_ip] = RH_CONFIG['opac_ip']
 
 # Path to ead xml
-config[:ead_path] = "public/fa"
+config[:ead_path] = RH_CONFIG['ead_path']
 
 # Path to our streaming server
-config[:rtmp_url] = "rtmp://192.168.251.84/vod"
+config[:rtmp_url] = RH_CONFIG['rtmp_url']
 
 # Local networks
-config[:local_networks] = [
-  "207.206.49.0/24"
-]
+config[:local_networks] = RH_CONFIG['local_networks']
 
 # Pin our hash to the global Rails configuration
 Rails.configuration.rockhall_config = config
