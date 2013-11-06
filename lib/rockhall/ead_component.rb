74e92e711404f0852e0a690505290a1ba38cb7d9
Adam Wead <amsterdamos@gmail.com>
2013-11-06 12:57:02 -0500

    Updating solr_ead and using localized customizations

--- lib/rockhall/ead_component.rb
+++ lib/rockhall/ead_component.rb
@@ -2,25 +2,31 @@ class Rockhall::EadComponent < SolrEad::Component
 
   include Rockhall::EadBehaviors
 
-  @terminology = SolrEad::Component.terminology
+  use_terminology SolrEad::Component
+
+  extend_terminology do |t|
+
+    # <odd> nodes
+    # These guys depend on what's in <head> so we do some xpathy stuff...
+    t.note(:path=>'odd[./head="General note"]/p', :index_as=>[:displayable])
+    t.accession(:path=>'odd[./head[starts-with(.,"Museum Accession")]]/p', :index_as=>[:displayable])
+    t.print_run(:path=>'odd[./head[starts-with(.,"Limited")]]/p', :index_as=>[:displayable])
+
+  end
 
   def to_solr(solr_doc = Hash.new)
     super(solr_doc)
-
-    # Alter our heading display if the title is blank
-    if self.title.first.blank?
-      heading = (solr_doc[Solrizer.solr_name("heading", :displayable)].to_s + self.unitdate.first.to_s)
-      Solrizer.insert_field(solr_doc, "heading", heading, :displayable ) 
-    end
-    
+    solr_doc.merge!({"text" => self.ng_xml.text})
+    Solrizer.insert_field(solr_doc, "format", "Archival Item", :symbol)
+    Solrizer.insert_field(solr_doc, "heading", heading_display(solr_doc), :displayable)
     Solrizer.insert_field(solr_doc, "location", location_display, :displayable)
     Solrizer.insert_field(solr_doc, "accession", ead_accession_range(self.accession.first), :searchable)
     Solrizer.insert_field(solr_doc, "language", get_language_from_code(self.langcode.first), :facetable)
-
-    solr_doc.merge!({"text" => [self.title, solr_doc[Solrizer.solr_name("parent_unittitles", :displayable)]].flatten })
   end
 
-  def location_display(locations = Array.new)
+  protected
+
+  def location_display locations = Array.new
     self.container_id.each do |id|
       line = String.new
       line << (self.find_by_xpath("//container[@id = '#{id}']/@type").text + ": ")
@@ -36,6 +42,20 @@ class Rockhall::EadComponent < SolrEad::Component
     return locations
   end
 
-  
+  def heading_display solr_doc
+    if self.title.first.blank?
+      self.unitdate.first.to_s unless self.unitdate.empty?
+    else
+      title_for_heading(solr_doc[Solrizer.solr_name("parent_unittitles", :displayable)]) unless solr_doc[Solrizer.solr_name("parent_unittitles", :displayable)].nil?
+    end
+  end
+
+  def title_for_heading parent_titles = Array.new
+    if parent_titles.length > 0
+      [parent_titles, self.title.first].join(" >> ")
+    else
+      self.title.first
+    end
+  end
 
 end
--- lib/rockhall/ead_document.rb
+++ lib/rockhall/ead_document.rb
@@ -2,19 +2,27 @@ class Rockhall::EadDocument < SolrEad::Document
 
   include Rockhall::EadBehaviors
 
-  @terminology = SolrEad::Document.terminology
+  use_terminology SolrEad::Document
 
   def to_solr(solr_doc = Hash.new)
     super(solr_doc)
-    solr_doc.merge!({"text"   => self.ng_xml.text})
+    solr_doc.merge!({"text" => self.ng_xml.text})
+    Solrizer.insert_field(solr_doc, "heading", heading_display, :displayable) unless self.title_num.empty?
+    Solrizer.insert_field(solr_doc, "format", "Archival Collection", :symbol)
     Solrizer.insert_field(solr_doc, "unitdate", ead_date_display, :displayable)
     Solrizer.insert_field(solr_doc, "language", get_language_from_code(self.langcode.first), :facetable )
     Solrizer.insert_field(solr_doc, "genre", self.genreform, :facetable)
     Solrizer.insert_field(solr_doc, "contributors", get_ead_names, :displayable)
     Solrizer.insert_field(solr_doc, "name", get_ead_names, :facetable)
     Solrizer.insert_field(solr_doc, "subject", self.subject, :displayable)
-    Solrizer.insert_field(solr_doc, "subject", get_ead_subject_facets, :facetable)
+    Solrizer.set_field(solr_doc, "subject", get_ead_subject_facets, :facetable)
     return solr_doc
+  end
+
+  protected
+
+  def heading_display
+    "Guide to the " + self.title.first + " (" + self.title_num.first + ")"
   end
 
 end
