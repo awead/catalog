9ddf551997c4e5142f05b0bdf859eea40a6178ac
Adam Wead <amsterdamos@gmail.com>
2013-11-14 13:30:14 -0500

    New Solrizer fields for HoldingsHelper

--- app/helpers/holdings_helper.rb
+++ app/helpers/holdings_helper.rb
@@ -2,23 +2,16 @@ module HoldingsHelper
 
   include Rockhall::Innovative
 
-  def opac_link(iii_id)
-    results = String.new
-    link = Rockhall::Innovative.link(iii_id.first.to_s)
-    results << "<h5>" + link_to("View in opac", link) + "</h5>"
-    return nil
-  end
-
   def show_full_holdings
-    show_holdings(@document, {:full=>TRUE})
+    show_holdings @document, {:full=>TRUE}
   end
 
-  def show_holdings(doc = @document, opts={})
-    if doc[:innovative_display]
-      case doc[:format]
+  def show_holdings doc = @document, opts={}
+    if doc.get Solrizer.solr_name("innovative", :displayable)
+      case doc.get(Solrizer.solr_name("format", :displayable))
         when "Website" then show_website_holdings(opts)
         when "Periodical" then show_periodical_holdings(doc, opts)
-        else show_all_holdings(doc, opts)
+        else show_all_holdings(doc.get(Solrizer.solr_name("innovative", :displayable)), opts)
       end
     end
   end
@@ -28,7 +21,6 @@ module HoldingsHelper
   end
 
   def show_periodical_holdings doc, opts
-    puts @document.inspect
     if opts[:full]
       render :partial => "holdings/show/periodical", :locals => { :holdings => build_holdings_array(doc) }
     else
@@ -36,21 +28,21 @@ module HoldingsHelper
     end
   end
 
-  def show_all_holdings doc, opts
+  def show_all_holdings id, opts
     if opts[:full]
-      content_tag(:div, nil, :class => "innovative_holdings", :id => doc[:innovative_display].first)
+      content_tag :div, nil, :class => "innovative_holdings", :id => id
     else
-      content_tag(:span, "checking status..." , :class => "innovative_status badge", :id => doc[:innovative_display].first)
+      content_tag :span, "checking status..." , :class => "innovative_status badge", :id => id
     end
   end
 
-  def build_holdings_array(doc, holdings = Array.new)
-    doc[:holdings_location_display].each do |code|
+  def build_holdings_array doc, holdings = Array.new
+    doc[Solrizer.solr_name("holdings_location", :displayable)].each do |code|
       if code.match(/^rh/)
         h = Rockhall::Holdings.new
-        index = doc[:holdings_location_display].index(code)
+        index = doc[Solrizer.solr_name("holdings_location", :displayable)].index(code)
         h.location    = location[code]
-        h.call_number = doc[:lc_callnum_display].first
+        h.call_number = doc.get(Solrizer.solr_name("lc_callnum", :displayable))
         h.status      = get_status(index)
         holdings << h
       end
@@ -58,13 +50,15 @@ module HoldingsHelper
     return holdings
   end
 
-  def render_holdings_link(doc = @document)
-    link_to("Click for Holdings", Rockhall::Innovative.link(doc[:innovative_display].first), { :target => "_blank"})
+  def render_holdings_link doc = @document
+    link_to "Click for Holdings", 
+      Rockhall::Innovative.link(doc.get(Solrizer.solr_name("innovative", :displayable))), 
+      {:target => "_blank"}
   end
 
-  def get_status(index)
-    unless @document[:holdings_status_display].nil?
-      status[@document[:holdings_status_display][index]]
+  def get_status index
+    unless @document[Solrizer.solr_name("holdings_status", :displayable)].nil?
+      status[@document[Solrizer.solr_name("holdings_status", :displayable)][index]]
     end
   end
 
