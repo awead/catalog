76613e623cc8c98243963dcbb5c5ecdbd6ff622f
Adam Wead <awead@rockhall.org>
2014-03-04 10:38:07 -0500

    Updating bookmarks layout

--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -2,9 +2,9 @@ module BookmarksHelper
 
   def render_bookmarks_index_view
     if current_or_guest_user.blank?
-      content_tag :p, t('blacklight.bookmarks.need_login')
+      content_tag :p, t("blacklight.bookmarks.need_login")
     elsif @document_list.blank?
-      content_tag :p, t('blacklight.bookmarks.no_bookmarks')
+      content_tag :p, t("blacklight.bookmarks.no_bookmarks")
     else
       render "bookmarks/list_view"
     end
